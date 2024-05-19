f20bc288acbbf1fac1443aa63a4a17005dddfa00
Jinwoo Je <jeff.jw.je@gmail.com>
2024-05-19 19:43:28 +0000

    hooks and context scritp update and WIP ContactMeSeciton

--- react-app/src/components/ContactMeSection.js
+++ react-app/src/components/ContactMeSection.js
@@ -10,9 +10,79 @@ import {
     Input,
     Select,
     Textarea,
-    VStac,
+    VStack,
+    FormErrorMessage,
 } from "@chakra-ui/react";
 import * as Yup from 'yup';
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import { useAlertContext } from "../context/alertContext";
+
+const LandingSection = () => {
+    const { isLoading, response, submit } = useSubmit();
+    const { onOpen } = useAlertContext();
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: "", 
+            email: "", 
+            type: "",
+            comment: "",
+        }, 
+        onSubmit: (values) => {
+            submit(values.firstName); 
+            formik.resetForm();
+        }, 
+        validationSchema: Yup.object({
+            firstName: Yup.string().required("Required"),
+            email: Yup.string().email("Invalid email address").required("Required"),
+            type: Yup.string(),
+            comment: Yup.string().required("Required").min(25, "Must be at least 25 characters"),
+        }),
+    });
+
+    return (
+        <FullScreenSection
+            isDarkBackground
+            backgroundColor="#512DA8"
+            py={16}
+            spacing={8}
+        >
+            <VStack>
+                <Heading>
+                </Heading>
+                <Box>
+                    <form onSubmit={formik.handleSubmit}>
+
+                        <VStack>
+                            <FormControl>
+                                <FormLabel></FormLabel>
+                                <Input/>
+                                <FormErrorMessage></FormErrorMessage>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel></FormLabel>
+                                <Input/>
+                                <FormErrorMessage></FormErrorMessage>                
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel></FormLabel>
+                                <Textarea/>
+                                <FormErrorMessage></FormErrorMessage>                
+                            </FormControl>
+                            <Button>
+                            </Button>
+                        </VStack>
+
+                    </form>
+
+                </Box>
+
+            </VStack>
+
+
+        </FullScreenSection>
+    )
+
+
+}
